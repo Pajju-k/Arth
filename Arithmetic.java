a45bedc2556f16dcd99a8e264835106d23f45702
Pajju-k <imprajwalsk@gmail.com>
2025-04-24 11:30:40 +0530

    save a file

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetic {
+
+    static int a = 100;
+    static int b = 50;
+
+    public static void main(String[] args) {
+        System.out.println("Addition is " + (a + b));
+        System.out.println("Subtraction is " + (a - b));
+    }
+}
